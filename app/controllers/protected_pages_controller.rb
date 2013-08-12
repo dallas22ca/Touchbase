b2afaa5183c289cb4e2d319bde4023e8cf977b1d
Dallas Read <dallas@excitereative.ca>
2013-08-12 11:28:04 -0300

    Redirect to welcome.

--- app/controllers/protected_pages_controller.rb
+++ app/controllers/protected_pages_controller.rb
@@ -1,7 +1,11 @@
 class ProtectedPagesController < ApplicationController
   def show
     permalink = params[:permalink]
-    permalink ||= "dashboard"
+    if current_user.step >= 4
+      permalink ||= "dashboard"
+    else
+      permalink ||= "welcome"
+    end
     render permalink
   end
 end
