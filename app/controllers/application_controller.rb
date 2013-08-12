e34307db108d13835fda599c5d43c56879fc570b
Dallas Read <dallas@excitereative.ca>
2013-08-12 13:29:45 -0300

    Time zone editable.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -56,6 +56,7 @@ protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :email, :password) }
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :time_zone) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password, :time_zone) }
   end
 
 end
