d5513e9112b0a0deea10dd15cec06d366b4c1c2c
Dallas Read <dallas@excitereative.ca>
2013-08-27 19:48:34 -0300

    log error.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,7 +16,8 @@ class PagesController < ApplicationController
   def submit
     @permalink = params[:permalink]
     tb = Tb.new(CONFIG["api_token"])
-    contacts = tb.add_contacts(params[:contact], { overwrite: true })
+    contacts = tb.add_contacts([params[:contact]], { overwrite: true })
+    p contacts
   end
   
   def option
