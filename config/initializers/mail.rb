b8dd35f88f6c48ab9f8fecf7aa9d3a439029ad3a
Dallas Read <dallas@excitereative.ca>
2013-09-07 11:39:44 -0300

    mail init

---
+++ config/initializers/mail.rb
@@ -0,0 +1,6 @@
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address:                 "127.0.0.1",
+  port:                    25,
+  enable_starttls_auto:    false
+}
