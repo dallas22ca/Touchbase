d1e3688a5c2fd1414ccfa0e9e9c2f5c5fd51b1cf
Dallas Read <dallas@excitereative.ca>
2014-04-15 16:06:09 -0300

    Log everyone out.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Touchbase::Application.config.session_store :cookie_store, key: '_touchbase_session'
+Touchbase::Application.config.session_store :cookie_store, key: '_touchbase_session_two'
